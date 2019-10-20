206f6513b792d01a642420ff52174739ef379bf7
dev-mev <40147757+dev-mev@users.noreply.github.com>
2019-10-19 18:34:12 -0700

    got definition lookup working

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,23 +1,47 @@
 <template>
-  <form @submit.prevent="submitLetters" class="search-form">
-    <label for="lettersInput">Enter your letters </label>
-    <input v-model="letters" id="lettersInput" required>
-    <button type="submit">Submit</button>
-  </form>
+  <div>
+    <form @submit.prevent="submitLetters" class="search-form">
+      <label for="lettersInput">Enter your letters </label>
+      <input v-model="lettersInput" id="lettersInput" required>
+      <button type="submit">Submit</button>
+    </form>
+
+    <SearchResults :words="words"/>
+  </div>
 </template>
 
 <script>
-import SearchResults from "@/components/SearchResults.vue"
+import SearchResults from "@/components/SearchResults.vue";
+import wordsApi from "@/services/api/wordsApi";
 
 export default {
   name: 'SearchBar',
+  components: {
+    SearchResults
+  },
   data() {
     return {
-      lettersInput: []
+      lettersInput: "",
+      words: []
     }
   },
+  methods: {
     submitLetters() {
-      console.log("Letters: " + lettersInput);
+      console.log("Letters: " + this.lettersInput);
+      let userLetters = this.lettersInput.toUpperCase().split("");
+      this.getWords(userLetters);
     },
+    getWords(letters) {
+      this.words = wordsApi.getWords(letters)
+    },
+  }
 }
 </script>
+
+<style scoped>
+.search-form {
+  text-align: center;
+  color: white;
+  margin-bottom: 1rem;
+}
+</style>
--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -1,42 +1,79 @@
 <template>
   <div class="search-results-container">
-      <div v-for="word in words" :key="word">
+    <button
+      class="search-result-button"
+      v-for="word in words"
+      :key="word"
+      v-bind:class="{
+        'active-word': activeWord === word
+      }"
+      v-on:click="accessDefinitions(word)"
+    >
       <div class="search-result">{{ word }}</div>
-      <div v-for="definition in definitions" :key="definition">
-          <p>{{ definition }}</p>
-      </div>
-      </div>
+        <div v-show="activeWord === word">
+          <p class="definition">{{ currentDefinition }}</p>
+        </div>
+    </button>
   </div>
 </template>
 
 <script>
+import definitionsApi from "@/services/api/definitionsApi";
+
 export default {
-  name: 'SearchResults',
-  props: ["lettersInput"],
+  name: "SearchResults",
+  props: {
+    words: Array
+  },
   data() {
     return {
-      words: [],
-      definitions: []
+      activeWord: "",
+      currentDefinition: ""
     }
   },
   methods: {
-    submitLetters() {
-      console.log("Letters: " + lettersInput);
-      let userLetters = this.lettersInput.toUpperCase().split("");
-      this.getWords(userLetters);
-    },
-    getWords(letters) {
-      this.words = wordsApi.getWords(letters)
-    },
     accessDefinitions(searchWord) {
+      this.activeWord = searchWord
+
       definitionsApi.getDefinitions(searchWord)
         .then(definition => {
           console.log(definition);
-          let definitionObject = {"word": searchWord, "definition": definition};
-          this.definitions.push(definitionObject);
+          // let definitionObject = {"word": searchWord, "definition": definition};
+          this.currentDefinition = definition;
         })
         .catch(error => console.error(error))
     }
   }
 }
 </script>
+
+<style scoped>
+.search-result-button {
+  width: 100%;
+  border: none;
+  padding: 0;
+  background-color: transparent;
+}
+
+.search-result {
+  background-color: #4E4473;
+  color: #A29DB2;
+  font-size: 20px;
+  height: 35px;
+  margin: 1px 0;
+  text-align: center;
+  padding-top: 10px;
+}
+
+.search-result:focus {
+  outline-color: #3DB57F;
+}
+
+.definition {
+  background-color: #443A66;
+  padding: .5rem;
+  color: #A29DB2;
+  margin: 1px;
+  text-align: left;
+}
+</style>
